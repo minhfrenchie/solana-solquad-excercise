9bf366bc151161342c97960d72ad5963b164eee2
minhfrenchie <KalynZubek514@gmail.com>
2024-07-26 10:04:08 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("9C8ouKeH8FPnk79CNu9zRxB9Bsg7Avfjz6VteXSZJXqE");
 
 #[program]
 pub mod solquad {
